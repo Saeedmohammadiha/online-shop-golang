24efe40d34a3210efcd997052f49491e8adc251b
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-13 19:38:01 +0330

    fix: change the import dto package to lowercase

--- services/Users.go
+++ services/Users.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/OnlineShop/dto/User"
+	"github.com/OnlineShop/dto/user"
 	"github.com/OnlineShop/models"
 	"github.com/OnlineShop/repository"
 	"github.com/OnlineShop/utils"
--- validation/UserValidation.go
+++ validation/UserValidation.go
@@ -1,7 +1,7 @@
 package validation
 
 import (
-	"github.com/OnlineShop/dto/User"
+	"github.com/OnlineShop/dto/user"
 	"github.com/go-ozzo/ozzo-validation"
 	"github.com/go-ozzo/ozzo-validation/is"
 )
