559a35ce7e31760e77d8f761285fba498f7f28f7
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-13 22:22:53 +0330

    feat: add dto for permission create service

---
+++ dto/Permission/PermissionCreateRequest.go
@@ -0,0 +1,5 @@
+package dto
+
+type PermissionCreateRequest struct {
+	Title string `json:"title,omitempty"`
+}
---
+++ dto/Permission/PermissionCreateResponse.go
@@ -0,0 +1,6 @@
+package dto
+
+type PermissionCreateResponse struct {
+	ID uint `json:"id"`
+	Title string `json:"title"`
+}
