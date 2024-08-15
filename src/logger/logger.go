03f02047d5e581add69950930ce9c979401f981b
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-15 23:16:22 +0330

    refactor: add formatting functions to logger package

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -12,6 +12,11 @@ type Ilogger interface {
 	Warn(msg string, args ...interface{})
 	Error(msg string, args ...interface{})
 	Fatal(msg string, args ...interface{})
+	Debugf(msg string, args ...interface{})
+	Infof(msg string, args ...interface{})
+	Warnf(msg string, args ...interface{})
+	Errorf(msg string, args ...interface{})
+	Fatalf(msg string, args ...interface{})
 	Sync() error
 }
 
@@ -40,15 +45,36 @@ func (l *Logger) Debug(msg string, args ...interface{}) {
 func (l *Logger) Info(msg string, args ...interface{}) {
 	l.Sugar.Infow(msg, args...)
 }
+
 func (l *Logger) Warn(msg string, args ...interface{}) {
 	l.Sugar.Warnw(msg, args...)
 }
+
 func (l *Logger) Error(msg string, args ...interface{}) {
 	l.Sugar.Errorw(msg, args...)
 }
+
 func (l *Logger) Fatal(msg string, args ...interface{}) {
 	l.Sugar.Fatalw(msg, args...)
 }
+
+func (l *Logger) Debugf(msg string, args ...interface{}) {
+	l.Sugar.Debugf(msg, args...)
+}
+
+func (l *Logger) Infof(msg string, args ...interface{}) {
+	l.Sugar.Infof(msg, args...)
+}
+func (l *Logger) Warnf(msg string, args ...interface{}) {
+	l.Sugar.Warnf(msg, args...)
+}
+func (l *Logger) Errorf(msg string, args ...interface{}) {
+	l.Sugar.Errorf(msg, args...)
+}
+func (l *Logger) Fatalf(msg string, args ...interface{}) {
+	l.Sugar.Fatalf(msg, args...)
+}
+
 func (l *Logger) Sync() error {
 	return l.Sugar.Sync()
 }
