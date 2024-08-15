cfe70e77ded0f393fea1bf24c34d9760d046b811
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-15 23:18:16 +0330

    refactor: add log and pass it to the service cunstructor

--- src/http/v1/V1.go
+++ src/http/v1/V1.go
@@ -1,13 +1,14 @@
 package v1
 
 import (
+	"github.com/OnlineShop/src/logger"
 	"github.com/OnlineShop/src/repository"
 	"github.com/OnlineShop/src/router"
 	"github.com/OnlineShop/src/services"
 )
 
-func RegisterRoutes(r router.IRouter, RepositoryFactory *repository.RepositoryFactory) {
+func RegisterRoutes(r router.IRouter, RepositoryFactory *repository.RepositoryFactory, log logger.Ilogger) {
 	v1Router := r.CreateSubRouter("/v1/api")
-	permissionService := services.NewPermissionService(RepositoryFactory.PermissionRepository)
+	permissionService := services.NewPermissionService(RepositoryFactory.PermissionRepository, log)
 	RegisterPermissionRoutes(v1Router, permissionService)
 }
