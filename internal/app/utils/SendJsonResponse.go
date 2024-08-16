613bcb6a3bf454e82ae2d57515785a7b2053d14e
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-16 17:41:33 +0330

    feat: add a function for response the json data to uesrs

---
+++ internal/app/utils/SendJsonResponse.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/OnlineShop/internal/pkg/logger"
+)
+
+func SendJsonResponse(w http.ResponseWriter, data interface{}, l logger.Ilogger) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		l.Error("failed to parse json to send response", "status code", http.StatusInternalServerError)
+		http.Error(w, "failed to parse json", http.StatusInternalServerError)
+	}
+	l.Info("the response has sent to user",
+		"status code", http.StatusOK,
+		"response", data,
+	)
+}
