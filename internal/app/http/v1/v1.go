aadd3e89019364d5f773e8698c1c7850c7ca43d2
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-16 17:40:18 +0330

    fix: add new  v1 to fix the problem with names

---
+++ internal/app/http/v1/v1.go
@@ -0,0 +1,14 @@
+package v1
+
+import (
+	"github.com/OnlineShop/internal/app/repositories"
+	"github.com/OnlineShop/internal/app/router"
+	"github.com/OnlineShop/internal/app/services"
+	"github.com/OnlineShop/internal/pkg/logger"
+)
+
+func RegisterRoutes(r router.IRouter, RepositoryFactory *repositories.RepositoryFactory, log logger.Ilogger) {
+	v1Router := r.CreateSubRouter("/v1/api")
+	permissionService := services.NewPermissionService(RepositoryFactory.PermissionRepository, log)
+	RegisterPermissionRoutes(v1Router, permissionService)
+}
