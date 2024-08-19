343dc7f1e03827bf2f3b36cbb50f2bdd0130b541
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-19 19:43:06 +0330

    refactor: change the auth validation file

--- internal/app/validation/auth_validation.go
+++ internal/app/validation/auth_validation.go
@@ -1,25 +1,40 @@
 package validation
 
 import (
+	"fmt"
+
 	dto "github.com/OnlineShop/internal/app/dto/auth"
 	"github.com/OnlineShop/internal/app/utils"
+	"github.com/OnlineShop/internal/pkg/logger"
 	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/go-ozzo/ozzo-validation/is"
 )
 
-type AuthValidation interface {
-	ValidateLogin(r dto.LoginRequest) error
+type IAuthValidation interface {
+	ValidateLogin(requestData *dto.LoginRequest) error
 }
 
-type AuthhV struct{}
-
-func NewAuthValidator() AuthValidation {
-	return &AuthhV{}
+type AuthValidation struct {
+	log logger.Ilogger
 }
 
-func (*AuthhV) ValidateLogin(r dto.LoginRequest) error {
+func NewAuthValidator(l logger.Ilogger) IAuthValidation {
+	return &AuthValidation{
+		log: l,
+	}
+}
 
-	return validation.ValidateStruct(&r,
-		validation.Field(&r.Email, validation.Required.Error("you must provide the email"), is.Email.Error("the email is invalid")),
-		validation.Field(&r.Password, validation.Required.Error("you must provide the password"), utils.NewValidatePassword(r.Password)))
+func (v *AuthValidation) ValidateLogin(requestData *dto.LoginRequest) error {
+
+	err := validation.ValidateStruct(requestData,
+		validation.Field(&requestData.Email, validation.Required.Error("you must provide the email"), is.Email.Error("the email is invalid")),
+		validation.Field(&requestData.Password, validation.Required.Error("you must provide the password"), utils.NewValidatePassword(requestData.Password)))
+
+	if err != nil {
+		v.log.Error("validation error accuared for login", err)
+		return fmt.Errorf("%s%w", utils.ErrValidationTag, err)
+	}
+
+	v.log.Info("login validation was successful")
+	return nil
 }
