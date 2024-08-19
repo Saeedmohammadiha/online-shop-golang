62a731887317f7e328ef525efdc9244036b10fb9
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-19 22:36:13 +0330

    refactor: refactor user repository package

--- internal/app/repositories/users.go
+++ internal/app/repositories/users.go
@@ -13,11 +13,11 @@ type IUserRepository interface {
 	Create(user *models.User) (*models.User, error)
 	Update(user *models.User) (*models.User, error)
 	Delete(userID int) error
-	FindById(userID int) (*models.User, error)
-	FindAll() ([]models.User, error)
-	FindByEmail(userEmail string) (*models.User, error)
-	FindByRoleIdes(roleIds []uint) (*[]models.Role, error)
+	GetById(userID int) (*models.User, error)
+	GetAll() (*[]models.User, error)
 	IsUserExists(email string) (*models.User, error)
+
+	//	FindByRoleIdes(roleIds []uint) (*[]models.Role, error)
 }
 
 type UserRepository struct {
@@ -36,68 +36,103 @@ func (r *UserRepository) IsUserExists(email string) (*models.User, error) {
 	err := r.Db.Where("email = ?", email).Take(&user).Error
 
 	if err != nil {
+		r.log.Error("failed to get user",
+			"db error:", err.Error(),
+			"data", email,
+		)
 		return nil, fmt.Errorf("%s%w", utils.ErrDatabaseTag, err)
 	}
+	r.log.Debug("successfully found the user",
+		"data", user,
+	)
 	return &user, nil
 
 }
 
-func (repo *UserRepository) Create(user *models.User) (*models.User, error) {
-	if err := repo.Db.Create(&user).Error; err != nil {
-		return nil, err
+func (r *UserRepository) Create(user *models.User) (*models.User, error) {
+	if err := r.Db.Create(user).Error; err != nil {
+		r.log.Error("failed to create user",
+			"db error:", err.Error(),
+			"data", user,
+		)
+		return nil, fmt.Errorf("%s%w", utils.ErrDatabaseTag, err)
 	}
+	r.log.Debug("successfully created the user",
+		"data", user,
+	)
 	return user, nil
 }
 
-func (repo *UserRepository) Update(user *models.User) (*models.User, error) {
-	if err := repo.Db.Model(&user).Updates(user).Error; err != nil {
-		return nil, err
+func (r *UserRepository) Update(user *models.User) (*models.User, error) {
+	if err := r.Db.Model(user).Updates(user).Error; err != nil {
+		r.log.Error("failed to update user",
+			"db error:", err.Error(),
+			"data", user,
+		)
+		return nil, fmt.Errorf("%s%w", utils.ErrDatabaseTag, err)
 	}
+	r.log.Debug("successfully updated the user",
+		"data", user,
+	)
 	return user, nil
 }
 
-func (repo *UserRepository) Delete(userID int) error {
-	if err := repo.Db.Where("ID = ?", userID).Delete(userID).Error; err != nil {
-		return err
+func (r *UserRepository) Delete(userID int) error {
+	if err := r.Db.Where("ID = ?", userID).Delete(userID).Error; err != nil {
+		r.log.Error("failed to delete user",
+			"db error:", err.Error(),
+			"data", userID,
+		)
+		return fmt.Errorf("%s%w", utils.ErrDatabaseTag, err)
 	}
+	r.log.Debug("successfully deleted the user",
+		"data", userID,
+	)
 	return nil
 }
 
-func (repo *UserRepository) FindAll() ([]models.User, error) {
+func (r *UserRepository) GetAll() (*[]models.User, error) {
 	var users []models.User
-	if err := repo.Db.Find(&users).Error; err != nil {
-		return nil, err
+	if err := r.Db.Find(&users).Error; err != nil {
+		r.log.Error("failed to get users",
+			"db error:", err.Error(),
+			"data", nil,
+		)
+		return nil, fmt.Errorf("%s%w", utils.ErrDatabaseTag, err)
 	}
-	//	var results []models.User
-	//results = append(results, users...)
-
-	return users, nil
+	r.log.Debug("successfully got all the users",
+		"data", users,
+	)
+	return &users, nil
 }
 
-func (repo *UserRepository) FindById(userID int) (*models.User, error) {
+func (r *UserRepository) GetById(userID int) (*models.User, error) {
 	var user models.User
-	if err := repo.Db.First(&user, userID).Error; err != nil {
-		return nil, err
+	if err := r.Db.First(&user, userID).Error; err != nil {
+		r.log.Error("failed to find user",
+			"db error:", err.Error(),
+			"data", userID,
+		)
+		return nil, fmt.Errorf("%s%w", utils.ErrDatabaseTag, err)
 	}
+	r.log.Debug("successfully found the user",
+		"data", user,
+	)
 	return &user, nil
 
 }
-func (repo *UserRepository) FindByEmail(userEmail string) (*models.User, error) {
-	var user models.User
-	if err := repo.Db.Where("Email = ?", userEmail).First(&user).Error; err != nil {
-		return nil, err
-	}
-	return &user, nil
 
-}
+// func (r *UserRepository) FindByRoleIdes(roleIds []uint) (*[]models.Role, error) {
+// 	var roles []models.Role
 
-func (repo *UserRepository) FindByRoleIdes(roleIds []uint) (*[]models.Role, error) {
-	var roles []models.Role
+// 	err := r.Db.Where("id IN ?", roleIds).Find(&roles).Error
+// 	if err != nil {
+// 		r.log.Error("failed to get user",
+// 			"db error:", err.Error(),
+// 			"data", email,
+// 		)
+// 		return nil, fmt.Errorf("%s%w", utils.ErrDatabaseTag, err)
+// 	}
 
-	err := repo.Db.Where("id IN ?", roleIds).Find(&roles).Error
-	if err != nil {
-		return nil, err
-	}
-
-	return &roles, nil
-}
+// 	return &roles, nil
+// }
