1182e8c1eaf0fd6e5df0b02d3d8501a586072493
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-19 19:47:33 +0330

    fix: problem with names

---
+++ internal/app/repositories/users.go
@@ -0,0 +1,103 @@
+package repositories
+
+import (
+	"fmt"
+
+	"github.com/OnlineShop/internal/app/models"
+	"github.com/OnlineShop/internal/app/utils"
+	"github.com/OnlineShop/internal/pkg/logger"
+	"gorm.io/gorm"
+)
+
+type IUserRepository interface {
+	Create(user *models.User) (*models.User, error)
+	Update(user *models.User) (*models.User, error)
+	Delete(userID int) error
+	FindById(userID int) (*models.User, error)
+	FindAll() ([]models.User, error)
+	FindByEmail(userEmail string) (*models.User, error)
+	FindByRoleIdes(roleIds []uint) (*[]models.Role, error)
+	IsUserExists(email string) (*models.User, error)
+}
+
+type UserRepository struct {
+	Db  *gorm.DB
+	log logger.Ilogger
+}
+
+func NewUserRepository(db *gorm.DB, l logger.Ilogger) IUserRepository {
+	l.Info("new user Repository is created")
+	return &UserRepository{Db: db, log: l}
+}
+
+func (r *UserRepository) IsUserExists(email string) (*models.User, error) {
+
+	var user models.User
+	err := r.Db.Where("email = ?", email).Take(&user).Error
+
+	if err != nil {
+		return nil, fmt.Errorf("%s%w", utils.ErrDatabaseTag, err)
+	}
+	return &user, nil
+
+}
+
+func (repo *UserRepository) Create(user *models.User) (*models.User, error) {
+	if err := repo.Db.Create(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (repo *UserRepository) Update(user *models.User) (*models.User, error) {
+	if err := repo.Db.Model(&user).Updates(user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (repo *UserRepository) Delete(userID int) error {
+	if err := repo.Db.Where("ID = ?", userID).Delete(userID).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *UserRepository) FindAll() ([]models.User, error) {
+	var users []models.User
+	if err := repo.Db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	//	var results []models.User
+	//results = append(results, users...)
+
+	return users, nil
+}
+
+func (repo *UserRepository) FindById(userID int) (*models.User, error) {
+	var user models.User
+	if err := repo.Db.First(&user, userID).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+
+}
+func (repo *UserRepository) FindByEmail(userEmail string) (*models.User, error) {
+	var user models.User
+	if err := repo.Db.Where("Email = ?", userEmail).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+
+}
+
+func (repo *UserRepository) FindByRoleIdes(roleIds []uint) (*[]models.Role, error) {
+	var roles []models.Role
+
+	err := repo.Db.Where("id IN ?", roleIds).Find(&roles).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &roles, nil
+}
