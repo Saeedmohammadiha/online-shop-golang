0aba2b1a25982ed24698c702f70769171b57d0c8
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-22 12:50:32 +0330

    refactor: add jwt to the auth usecase

--- internal/app/http/v1/auth.go
+++ internal/app/http/v1/auth.go
@@ -6,12 +6,14 @@ import (
 	"github.com/OnlineShop/internal/app/services"
 	"github.com/OnlineShop/internal/app/usecases"
 	"github.com/OnlineShop/internal/app/validation"
+	internaljwt "github.com/OnlineShop/internal/pkg/jwt"
 	"github.com/OnlineShop/internal/pkg/logger"
 )
 
 func RegisterAuthRoutes(r router.IRouter, RepositoryFactory repositories.RepositoryFactory, l logger.Ilogger) {
+	j := internaljwt.New(l)
 	authValidator := validation.NewAuthValidation(l)
-	authUsecase := usecases.NewAuthUsecase(RepositoryFactory.UserRepository, authValidator, l)
+	authUsecase := usecases.NewAuthUsecase(RepositoryFactory.UserRepository, authValidator, j, l)
 	authService := services.NewAuthService(authUsecase, l)
 
 	r.RegisterRoute("POST", "/login", authService.Login)
