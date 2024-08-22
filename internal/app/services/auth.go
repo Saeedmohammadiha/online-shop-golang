44337e705030dea465e4e8d21d2b186837704712
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-22 12:54:39 +0330

    feat: add the refresh token service

--- internal/app/services/auth.go
+++ internal/app/services/auth.go
@@ -3,7 +3,6 @@ package services
 import (
 	"net/http"
 
-	dto "github.com/OnlineShop/internal/app/dto/auth"
 	"github.com/OnlineShop/internal/app/usecases"
 	"github.com/OnlineShop/internal/app/utils"
 
@@ -12,97 +11,46 @@ import (
 
 type IAuthService interface {
 	Login(w http.ResponseWriter, r *http.Request)
+	Refresh(w http.ResponseWriter, r *http.Request)
 }
 type AuthService struct {
 	authUsecase usecases.IAuthUsecases
-	log         logger.Ilogger
+
+	log logger.Ilogger
 }
 
 func NewAuthService(u usecases.IAuthUsecases, l logger.Ilogger) IAuthService {
 	l.Info("new Auth service is created")
-	return &AuthService{authUsecase: u, log: l}
+	return &AuthService{
+		authUsecase: u,
+		log:         l,
+	}
 }
 
 func (a *AuthService) Login(w http.ResponseWriter, r *http.Request) {
 
-	//get data from the context
 	ctx := r.Context()
-	var requestBody dto.LoginRequest
-	if err := utils.GetValueFromCtx(ctx, utils.REQUEST_BODY, &requestBody, a.log); err != nil {
-		utils.SendErrorResponse(ctx, err, w, a.log)
-	}
 
-	//get the response or err
-	responseData, err := a.authUsecase.Login(&requestBody)
+	responseData, err := a.authUsecase.Login(ctx)
 	if err != nil {
 		utils.SendErrorResponse(ctx, err, w, a.log)
 		return
 	}
 
 	utils.SendSuccessResponse(w, responseData, a.log)
-	return
 
 }
 
-// func (a *Auth) AuthMiddleware(next http.Handler, resourceId int) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (a *AuthService) Refresh(w http.ResponseWriter, r *http.Request) {
 
-// 		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
+	ctx := r.Context()
 
-// 		claims := utils.JWTClaim{}
-// 		if len(authHeader) != 2 {
-// 			fmt.Println("Malformed token")
-// 			w.WriteHeader(http.StatusUnauthorized)
-// 			w.Write([]byte("Malformed Token"))
-// 		} else {
-// 			jwtToken := authHeader[1]
-// 			_, err := jwt.ParseWithClaims(jwtToken, claims, func(token *jwt.Token) (interface{}, error) {
-// 				return []byte(utils.GetEnv("TOKEN_SECRET")), nil
-// 			})
-// 			if err != nil {
-// 				http.Error(w, "Unexpected signing method", http.StatusUnauthorized)
-// 				return
-// 			}
-// 		}
-// 		//ctx := context.WithValue(r.Context(), "claims", claims)
-// 		next.ServeHTTP(w, r)
-// 	})
+	responseData, err := a.authUsecase.Refresh(ctx)
+	if err != nil {
+		utils.SendErrorResponse(ctx, err, w, a.log)
+		return
+	}
 
-// }
-
-// func (a *Auth) GaurdMiddleware(next http.Handler, resourceId int) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-// 		//check if the user has the access
-// 		permission, err := a.p.FindByRoleAndResource(1, resourceId)
-// 		if err != nil {
-// 			fmt.Println("Error querying permission:", err)
-
-// 		}
-
-// 		switch r.Method {
-// 		case "GET":
-// 			if !permission.Read {
-// 				http.Error(w, "you don't have access", http.StatusUnauthorized)
-// 				return
-// 			}
-// 		case "POST":
-// 			if !permission.Create {
-// 				http.Error(w, "you don't have access", http.StatusUnauthorized)
-// 				return
-// 			}
-// 		case "PUT":
-// 			if !permission.Update {
-// 				http.Error(w, "you don't have access", http.StatusUnauthorized)
-// 				return
-// 			}
-// 		case "DELETE":
-// 			if !permission.Delete {
-// 				http.Error(w, "you don't have access", http.StatusUnauthorized)
-// 				return
-// 			}
-
-// 		}
-// 		next.ServeHTTP(w, r)
-// 	})
-// }
+	utils.SendSuccessResponse(w, responseData, a.log)
+	//TODO: add the invalide tokens in the logout service and if the user changed his password
+}
