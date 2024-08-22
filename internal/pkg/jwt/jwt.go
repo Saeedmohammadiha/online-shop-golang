d68ef6df1a0b2cdbc6f28df88cb31cdf55be3f8c
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-22 12:47:55 +0330

    refactor: change the jwt times

--- internal/pkg/jwt/jwt.go
+++ internal/pkg/jwt/jwt.go
@@ -1,8 +1,11 @@
 package internaljwt
 
 import (
+	"fmt"
 	"os"
+	"time"
 
+	"github.com/OnlineShop/internal/app/utils"
 	"github.com/OnlineShop/internal/pkg/logger"
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -10,6 +13,7 @@ import (
 type IJwtToken interface {
 	GenerateAccessToken(userID int) (*string, error)
 	GenerateRefreshToken(userID int) (*string, error)
+	DecodeToken(tokenString string) (*JWTClaim, error)
 }
 
 type JWTClaim struct {
@@ -31,6 +35,9 @@ func (j *JwtToken) GenerateAccessToken(userID int) (*string, error) {
 
 	var claims = &JWTClaim{
 		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(15 * time.Minute)), // Access token expires in 15 minutes
+		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
@@ -41,7 +48,7 @@ func (j *JwtToken) GenerateAccessToken(userID int) (*string, error) {
 			"user", userID,
 			"claim", claims,
 		)
-		return nil, err
+		return nil, fmt.Errorf("%s%w", utils.ErrTokenGenerationTag, err)
 
 	}
 	j.log.Debug("a new access token is generated",
@@ -55,6 +62,9 @@ func (j *JwtToken) GenerateAccessToken(userID int) (*string, error) {
 func (j *JwtToken) GenerateRefreshToken(userID int) (*string, error) {
 	var claims = &JWTClaim{
 		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(60 * time.Minute)), // Access token expires in 15 minutes
+		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
@@ -65,7 +75,7 @@ func (j *JwtToken) GenerateRefreshToken(userID int) (*string, error) {
 			"user", userID,
 			"claim", claims,
 		)
-		return nil, err
+		return nil, fmt.Errorf("%s%w", utils.ErrTokenGenerationTag, err)
 	}
 
 	j.log.Debug("a new refresh token is generated",
@@ -74,4 +84,36 @@ func (j *JwtToken) GenerateRefreshToken(userID int) (*string, error) {
 		"claim", claims,
 	)
 	return &RefreshToken, nil
+}
+
+func (j *JwtToken) DecodeToken(tokenString string) (*JWTClaim, error) {
+	claims := &JWTClaim{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("TOKEN_SECRET")), nil
+	})
+
+	if err != nil {
+		j.log.Error("failed to parse the token ",
+			"token", tokenString,
+		)
+		return nil, fmt.Errorf("%s%w", utils.ErrTokenDecodingTag, err)
+	}
+
+	if token.Valid {
+		j.log.Debug("the token is valid and parsed successfully",
+			"token", tokenString,
+			"decoded token", token,
+			"claims", claims,
+		)
+		return claims, nil
+
+	} else {
+		j.log.Error("the token is not valid",
+			"token", tokenString,
+			"decoded token", token,
+		)
+		return nil, fmt.Errorf("%s%w", utils.ErrTokenInvalidTag, err)
+	}
+
 }
