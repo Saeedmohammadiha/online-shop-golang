051b6a01beae368cf98f3663343706662c8c5dd1
saeedmohammadiha <saeed1440124@gmail.com>
2024-08-23 15:46:01 +0330

    refactor: register the all routes of the user

--- internal/app/http/v1/users.go
+++ internal/app/http/v1/users.go
@@ -17,9 +17,9 @@ func RegisterUsersRoutes(r router.IRouter, RepositoryFactory repositories.Reposi
 	usersService := services.NewUserService(usersUsecase, l)
 
 	usersRouter := r.CreateSubRouter("/users")
-	//	usersRouter.RegisterRoute("GET", "", middlewares.AuthProtect(middlewares.GuardRoute(usersService.GetAll, []string{"readPermissions"}, l), RepositoryFactory.UserRepository, l))
+	usersRouter.RegisterRoute("GET", "", middlewares.AuthProtect(middlewares.GuardRoute(usersService.GetAll, []string{"readPermissions"}, l), RepositoryFactory.UserRepository, l))
 	usersRouter.RegisterRoute("POST", "", middlewares.AuthProtect(middlewares.GuardRoute(usersService.Create, []string{"createPermission"}, l), RepositoryFactory.UserRepository, l))
-	//	usersRouter.RegisterRoute("PUT", "", middlewares.AuthProtect(middlewares.GuardRoute(usersService.Update, []string{"updatePermission"}, l), RepositoryFactory.UserRepository, l))
-	//	usersRouter.RegisterRoute("GET", "/{id}", middlewares.AuthProtect(middlewares.GuardRoute(usersService.GetById, []string{"readPermissions"}, l), RepositoryFactory.UserRepository, l))
-	//	usersRouter.RegisterRoute("DELETE", "/{id}", middlewares.AuthProtect(middlewares.GuardRoute(usersService.Delete, []string{"deletePermission"}, l), RepositoryFactory.UserRepository, l))
+	usersRouter.RegisterRoute("PUT", "", middlewares.AuthProtect(middlewares.GuardRoute(usersService.Update, []string{"updatePermission"}, l), RepositoryFactory.UserRepository, l))
+	usersRouter.RegisterRoute("GET", "/{id}", middlewares.AuthProtect(middlewares.GuardRoute(usersService.GetById, []string{"readPermissions"}, l), RepositoryFactory.UserRepository, l))
+	usersRouter.RegisterRoute("DELETE", "/{id}", middlewares.AuthProtect(middlewares.GuardRoute(usersService.Delete, []string{"deletePermission"}, l), RepositoryFactory.UserRepository, l))
 }
